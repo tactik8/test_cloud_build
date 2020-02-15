36527c16e757a542d6b0ac85ea0e17766255f109
tactik8 <admin@tactik8.com>
2020-02-15 18:20:50 +0000

    mm

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
-from flask import Flask
-import flask_admin
-from flask_admin.contrib import appengine
-from google.appengine.ext import db
-from google.appengine.ext import ndb
+import datetime
 
-# Create application
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# Create dummy secrey key so we can use sessions
-app.config['SECRET_KEY'] = '123456790'
 
-admin = flask_admin.Admin(app, name="Admin")
-
-# Flask views
 @app.route('/')
-def index():
-    return '<a href="/admin/">Click me to get to Admin!</a>'
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
-class Car(db.Model):
-    owner = db.StringProperty()
-    make_model = db.StringProperty()
-    indexed_int = db.IntegerProperty()
-    unindexed_int = db.IntegerProperty(indexed=False)
+    return render_template('index.html', times=dummy_times)
 
-class Tire(db.Model):
-    car = db.ReferenceProperty(Car)
-    position = db.StringProperty()
-
-class House(ndb.Model):
-    address = db.StringProperty()
-    json_property = ndb.JsonProperty()
-    indexed_int = ndb.IntegerProperty()
-    unindexed_int = ndb.IntegerProperty(indexed=False)
-    text_property = ndb.TextProperty()
-    datetime_property = ndb.DateTimeProperty()
-
-class Room(ndb.Model):
-    house = ndb.KeyProperty(kind=House)
-    name = ndb.StringProperty()
-
-admin.add_view(appengine.ModelView(Car))
-admin.add_view(appengine.ModelView(Tire))
-admin.add_view(appengine.ModelView(House))
-admin.add_view(appengine.ModelView(Room))
 
 if __name__ == '__main__':
-
-    # Start app
-    app.run(debug=True)
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='0.0.0.0', port=8080, debug=True)
